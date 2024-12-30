e4eb32d1d8c22ebf2512e22ac52a0526b972f948
basti <bs117@hdm-stuttgart.de>
2024-12-30 17:00:32 +0100

    Optimized ladder movement and code

--- Assets/Scripts/Environment/Interactive/LadderMovement.cs
+++ Assets/Scripts/Environment/Interactive/LadderMovement.cs
@@ -2,94 +2,78 @@ using UnityEngine;
 
 public class LadderMovement : MonoBehaviour
 {
-    // * References to player components
-    private HeroMovementController movementController;
-    private Rigidbody2D playerRb;
-    
-    // * Track ladder movement state
+    [SerializeField] private float climbSpeed = 8f;
+    private LadderController controller;
     private bool isMovingUp = false;
     private bool isMovingDown = false;
     private bool isOnLadder = false;
 
-    [SerializeField] private float climbSpeed = 8f;
-
-    // * Get player components at start
     private void Start()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        if (player != null)
+        controller = new LadderController();
+        controller.Initialize();
+        UIEvents.OnControlButtonStateChanged += HandleControlInput;
+    }
+
+    private void OnDestroy()
+    {
+        UIEvents.OnControlButtonStateChanged -= HandleControlInput;
+    }
+
+    private void HandleControlInput(string buttonType, bool isPressed)
+    {
+        if (!isOnLadder) return;
+
+        switch (buttonType.ToLower())
         {
-            movementController = player.GetComponent<HeroMovementController>();
-            playerRb = player.GetComponent<Rigidbody2D>();
+            case "up":
+                isMovingUp = isPressed;
+                if (!isPressed)
+                {
+                    controller.UpdateMovement(0, 0);
+                }
+                break;
+            case "down":
+                isMovingDown = isPressed;
+                if (!isPressed)
+                {
+                    controller.UpdateMovement(0, 0);
+                }
+                break;
         }
     }
 
-    // * Enable ladder movement when player enters
+    private void Update()
+    {
+        if (isOnLadder)
+        {
+            float direction = 0;
+            if (isMovingUp) direction = 1;
+            if (isMovingDown) direction = -1;
+
+            controller.UpdateMovement(direction, climbSpeed);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             isOnLadder = true;
-            playerRb.gravityScale = 0;
+            controller.StartClimbing();
+            Debug.Log("Player entered ladder");
         }
     }
 
-    // * Disable ladder movement when player exits
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             isOnLadder = false;
-            playerRb.gravityScale = 1;
+            controller.StopClimbing();
             isMovingUp = false;
             isMovingDown = false;
+            Debug.Log("Player exited ladder");
         }
-    }
-
-    // * Check for ladder movement every frame
-    private void Update()
-    {
-        if (isOnLadder)
-        {
-            HandleLadderMovement();
-        }
-    }
-
-    // * Process vertical movement on ladder
-    private void HandleLadderMovement()
-    {
-        float verticalMovement = 0;
-        
-        if (isMovingUp) verticalMovement = 1;
-        if (isMovingDown) verticalMovement = -1;
-
-        playerRb.velocity = new Vector2(playerRb.velocity.x, verticalMovement * climbSpeed);
-    }
-
-    // * Button input handlers for ladder movement
-    public void OnUpButtonDown()
-    {
-        if (isOnLadder)
-        {
-            isMovingUp = true;
-        }
-    }
-
-    public void OnUpButtonUp()
-    {
-        isMovingUp = false;
-    }
-
-    public void OnDownButtonDown()
-    {
-        if (isOnLadder)
-        {
-            isMovingDown = true;
-        }
-    }
-
-    public void OnDownButtonUp()
-    {
-        isMovingDown = false;
     }
 }
