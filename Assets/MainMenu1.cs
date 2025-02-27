704f6440e482e28e73a182281239f4a5bfa34001
Biran Arslan <57496276+arslanbri@users.noreply.github.com>
2025-02-27 21:28:59 +0100

    OptionsScreen and Menu

--- Assets/MainMenu1.cs
+++ Assets/MainMenu1.cs
@@ -5,7 +5,24 @@ public class MainMenu1 : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("Scenes/Levels/Level 1"); // Achte auf den korrekten Namen!
+        SceneManager.LoadSceneAsync("Scenes/Levels/Level 1"); 
+    }
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false; 
+        #else
+            Application.Quit(); 
+        #endif
+    }
+    public void OptionsGame()
+    {
+        SceneManager.LoadSceneAsync("Scenes/Menus/MenuOptions");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadSceneAsync("Scenes/Menus/Menu");
     }
 }
+
 
